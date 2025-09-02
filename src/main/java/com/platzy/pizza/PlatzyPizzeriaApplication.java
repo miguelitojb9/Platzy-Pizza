02ab57ff7f8966c3403fbd77b114366e79840e87
miguelitojb9 <miguelitojb9@gmail.com>
2025-09-02 07:00:04 +0200

    feat: initialize  project with basic configuration

---
+++ src/main/java/com/platzy/pizza/PlatzyPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.platzy.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatzyPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatzyPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platzy/pizza/PlatzyPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzy.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatzyPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
