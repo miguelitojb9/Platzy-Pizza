efd20831b30539c5ad80bbfbc2f6b0dc8ab21601
miguelitojb9 <miguelitojb9@gmail.com>
2025-09-16 15:05:17 +0200

    Add: -Page and Sorting Repository

--- src/main/java/com/platzy/pizza/service/PizzaService.java
+++ src/main/java/com/platzy/pizza/service/PizzaService.java
@@ -3,26 +3,43 @@ package com.platzy.pizza.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.platzy.pizza.persistence.entity.PizzaEntity;
+import com.platzy.pizza.persistence.repository.PizzaPagSortRepository;
 import com.platzy.pizza.persistence.repository.PizzaRepository;
 
 @Service
 public class PizzaService {
     private final PizzaRepository pizzaRepository;
+    private final PizzaPagSortRepository pizzaPagSortRepository;
 
     @Autowired
-    public PizzaService(PizzaRepository pizzaRepository) {
+    public PizzaService(PizzaRepository pizzaRepository, PizzaPagSortRepository pizzaPagSortRepository) {
         this.pizzaRepository = pizzaRepository;
+        this.pizzaPagSortRepository = pizzaPagSortRepository;
     }
 
     public List<PizzaEntity> getAll(){
         return this.pizzaRepository.findAll();
     }
 
-    public List<PizzaEntity> getAvailable(){
-        return this.pizzaRepository.findAllByAvailableTrueOrderByPrice();
+    public Page<PizzaEntity> getAllPagSort(int page, int elements){
+        Pageable pageRequest = PageRequest.of(page, elements);
+        return this.pizzaPagSortRepository.findAll(pageRequest);
+    }
+
+    // public List<PizzaEntity> getAvailable(){
+    //     return this.pizzaRepository.findAllByAvailableTrueOrderByPrice();
+    // }
+    public Page<PizzaEntity> getAvailable(int page, int elements, String sortBy, String sortDirection){
+        Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
+        Pageable pageRequest = PageRequest.of(page, elements, sort);
+        return this.pizzaPagSortRepository.findByAvailableTrue(pageRequest);
     }
 
     public PizzaEntity getAvailableByName(String name){
--- src/main/java/com/platzy/pizza/web/controller/PizzaController.java
+++ src/main/java/com/platzy/pizza/web/controller/PizzaController.java
@@ -3,6 +3,7 @@ package com.platzy.pizza.web.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.platzy.pizza.persistence.entity.PizzaEntity;
@@ -32,9 +34,20 @@ public class PizzaController {
         return ResponseEntity.ok(this.pizzaService.getAll());
     }
 
+    @GetMapping("/pag-sort")
+    public ResponseEntity<Page<PizzaEntity>> getAll(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int elements){
+        return ResponseEntity.ok(this.pizzaService.getAllPagSort(page, elements));
+    }
+
     @GetMapping("/available")
-    public ResponseEntity<List<PizzaEntity>> getAvailable(){
-        return ResponseEntity.ok(this.pizzaService.getAvailable());
+    public ResponseEntity<Page<PizzaEntity>> getAvailable(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int elements,
+        @RequestParam(defaultValue = "price") String sort,
+        @RequestParam(defaultValue = "ASC") String sortDirection){
+        return ResponseEntity.ok(this.pizzaService.getAvailable(page, elements, sort, sortDirection));
     }
 
     @GetMapping("/available/{name}")
